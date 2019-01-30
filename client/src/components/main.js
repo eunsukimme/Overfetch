ce31c3f418c10ef325e6f7ddc2d86389cf8fdde0
eunsukimme <eunsu.dev@gmail.com>
2019-01-31 02:09:23 +0900

    Edit most champion requesting function

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -7,7 +7,8 @@ export class Main extends React.Component {
         super(props);
         this.state = {
             name: '기운찬곰',
-            tag: '311530'
+            tag: '311530',
+            most: []
         }
         this.handleChangeName = this.handleChangeName.bind(this);
         this.handleChangeTag = this.handleChangeTag.bind(this);
@@ -19,16 +20,30 @@ export class Main extends React.Component {
         const cors = 'https://cors-anywhere.herokuapp.com/';
         const url = cors + 'https://playoverwatch.com/ko-kr/career/pc/'
                     +this.state.name+'-'+this.state.tag;
-        /*const response = await fetch(url);
-        console.log(response);
-        const jsonResponse = await response.text();
-        console.log(jsonResponse);*/
+
         request(url, (error, response, body) => {
             if(error) console.warn(error);
-
-            console.log(body);
             const $ = cheerio.load(body);
-            console.log($('div .ProgressBar-bar', 'div', $('#quickplay')).text());
+            
+            const champion = {};
+            // 빠른대전의 현재 보이는 모든 기록을 가져온다
+            //console.log($('#quickplay').find('.is-active').text());
+
+            // 현재 기준(플레이시간) 상위 영웅 리스트를 가져온다
+            const list = $('#quickplay').find($('.is-active')).find('.ProgressBar');
+            list.each((i, el) =>{
+                const championName = $(el).find('div > div').find('.ProgressBar-title').text();
+                const championValue = $(el).find('div > div').find('.ProgressBar-description').text();
+                champion[championName] = championValue;
+
+                const temp = this.state.most;
+                temp.push(<li>{championName}  -  {championValue}</li>);
+                this.setState({
+                    most: temp
+                });
+
+            });
+            console.log(champion);
         })
 
     }
@@ -52,12 +67,15 @@ export class Main extends React.Component {
                     <h1>닉네임</h1>
                     <input placeholder="닉네임" type='text' onChange={this.handleChangeName}/>
                     <h1>배틀태그</h1>
-                    <input placeholder="tag" type='text' onChange={this.handleChangeTag}/>
+                    <input placeholder="배틀태그" type='text' onChange={this.handleChangeTag}/>
                     <button type='submit'>Submit</button>
                 </form>
                 <div id='data-field'>
                     <h1>Most Champion</h1>
                     <div id='most-champion'>
+                        <ol>
+                            {this.state.most}
+                        </ol>
                     </div>
                 </div>
             </div>
